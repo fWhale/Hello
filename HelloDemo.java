8e675edc58582cab1a380f8f1fcf71aae2c5f080
abcdefg1234 <abcdefg1234@126.com>
2016-08-07 00:24:49 +0800

    changed 1st time

--- HelloDemo.java
+++ HelloDemo.java
@@ -1,5 +1,7 @@
 public class HelloDemo{
   public static void main(String[] args){
     System.out.println("Hello ! My first Git Project ! ");
-  }
+    System.out.println("Local Edition...");
+}
+
 }
